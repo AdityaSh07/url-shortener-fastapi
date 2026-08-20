4a5e3585ac80c914620c66767efced3368be6da4
Aditya Sharma <sharmadevaditya95@gmail.com>
2026-08-20 21:24:13 +0530

    dockerized the URL shortener

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
-    base_url: str
+    base_url: str = "http://localhost:8000" # base_url if not provided for testing purposes.
 
     model_config = SettingsConfigDict(
         env_file=".env",
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 from app.api.routes import router
 from app.database.database import engine, Base
@@ -20,4 +21,15 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+
+@app.get("/health", include_in_schema=False)
+async def health_check():
+    return {"status": "ok"}
+
+
 app.include_router(router)
+
+
+@app.get("/", include_in_schema=False)
+async def home():
+    return FileResponse("app/templates/index.html")
